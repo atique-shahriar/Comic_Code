a2d67fd0e00ba6d7e508c485ac32358084209780
atique-shahriar <you@example.comsatique06@gmail.com>
2024-03-03 19:26:32 +0600

    Profile indicator red and green functionality added

--- main.js
+++ main.js
@@ -12,38 +12,78 @@ const displayForumPost = (forumPosts) => {
 
     const forumContainer = document.getElementById("forumContainer");
     forumPosts.forEach(post => {
+        console.log(post);
+
         const singleForum = document.createElement("div");
         singleForum.classList.add("singleForum", "bg-slate-200", "p-6", "flex", "gap-4", "rounded-xl");
-        singleForum.innerHTML = `
+        if (post.isActive === true) {
+            singleForum.innerHTML = `
                     <div>
                         <div class="avatar indicator">
-                            <span class="indicator-item badge badge-secondary  border-white"></span>
+                            <span class="indicator-item badge badge-secondary border-white bg-green-500"></span>
                             <div class="w-20 h-20 rounded-lg">
                                 <img alt="Tailwind CSS examples"
-                                    src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                                    src="${post.image}" />
                             </div>
                         </div>
                     </div>
-                    <div>
-                        <p><span># Music</span><span class="ml-10">Author : Awlad Hossain</span></p>
-                        <h4 class="text-3xl font-extrabold colorText my-4">10 Kids Unaware of Their Halloween Costume
+                    <div class="w-full">
+                        <p><span># ${post.category}</span><span class="ml-10">Author : ${post.author.name}</span></p>
+                        <h4 class="text-2xl font-extrabold colorText my-4">${post.title}
                         </h4>
-                        <p class="text-[#12132d9d] border-b-2 border-[#12132d9d] border-dashed pb-4">It’s one thing to
-                            subject yourself to ha Halloween costume mishap because, hey that’s your prerogative</p>
+                        <p class="text-[#12132d9d] border-b-2 border-[#12132d9d] border-dashed pb-4">${post.description}</p>
                         <div class="mt-4 flex justify-between items-center">
                             <div>
                                 <p class="text-[#12132d9d]">
-                                    <span class="mr-5"><i class="fa-regular fa-message mr-4"></i>560</span>
-                                    <span class="mr-5"><i class="fa-regular fa-eye mr-4"></i>560</span>
-                                    <span class="mr-5"><i class="fa-regular fa-clock mr-4"></i>5 min</span>
+                                    <span class="mr-5"><i class="fa-regular fa-message mr-4"></i>${post.comment_count}</span>
+                                    <span class="mr-5"><i class="fa-regular fa-eye mr-4"></i>${post.view_count}</span>
+                                    <span class="mr-5"><i class="fa-regular fa-clock mr-4"></i>${post.posted_time} min</span>
                                 </p>
                             </div>
                             <div>
-                                <i class="fa-regular fa-envelope-open bg-green-600 p-2 rounded-[100%] text-white"></i>
+                               <button onclick="markRead(${post.title},${post.view_count})"> <i class="fa-regular fa-envelope-open bg-green-600 p-2 rounded-[100%] text-white"></i></button>
                             </div>
                         </div>
                     </div>
-        `
+`
+        }
+        else {
+            singleForum.innerHTML = `
+                    <div>
+                        <div class="avatar indicator">
+                            <span class="indicator-item badge badge-secondary border-white bg-red-500"></span>
+                            <div class="w-20 h-20 rounded-lg">
+                                <img alt="Tailwind CSS examples"
+                                    src="${post.image}" />
+                            </div>
+                        </div>
+                    </div>
+                    <div class="w-full">
+                        <p><span># ${post.category}</span><span class="ml-10">Author : ${post.author.name}</span></p>
+                        <h4 class="text-2xl font-extrabold colorText my-4">${post.title}
+                        </h4>
+                        <p class="text-[#12132d9d] border-b-2 border-[#12132d9d] border-dashed pb-4">${post.description}</p>
+                        <div class="mt-4 flex justify-between items-center">
+                            <div>
+                                <p class="text-[#12132d9d]">
+                                    <span class="mr-5"><i class="fa-regular fa-message mr-4"></i>${post.comment_count}</span>
+                                    <span class="mr-5"><i class="fa-regular fa-eye mr-4"></i>${post.view_count}</span>
+                                    <span class="mr-5"><i class="fa-regular fa-clock mr-4"></i>${post.posted_time} min</span>
+                                </p>
+                            </div>
+                            <div>
+                            <button onclick="markRead(${post.title},${post.view_count})"> <i class="fa-regular fa-envelope-open bg-green-600 p-2 rounded-[100%] text-white"></i></button>
+                            </div>
+                        </div>
+                    </div>
+`
+        }
         forumContainer.appendChild(singleForum);
+
     });
 }
+
+
+
+
+
